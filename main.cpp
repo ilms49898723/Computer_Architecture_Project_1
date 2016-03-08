00126e2fa90a090c3c6542976615f08ac215e522
LittleBird <ilms49898723@gmail.com>
2016-03-08 19:59:14 +0900

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2016/03/08
+ *      Author: LittleBird
+ */
+
+#include <cstdio>
+
+int main(int argc, char **argv) {
+    printf("Program Entry\n");
+    return 0;
+}
