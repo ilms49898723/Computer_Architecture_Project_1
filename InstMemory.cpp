9885376def8c1fe3cfab71f9aeb150e1d22ed4cb
LittleBird <ilms49898723@gmail.com>
2016-03-11 00:47:28 +0900

    Add class InstMemory

---
+++ InstMemory.cpp
@@ -0,0 +1,74 @@
+/*
+ * InstMemory.cpp
+ *
+ *  Created on: 2016/03/10
+ *      Author: LittleBird
+ */
+
+#include "InstMemory.h"
+
+LB::InstMemory::InstMemory() {
+    pc = 0U;
+    memset(reg, 0, sizeof(unsigned) * 32);
+}
+
+LB::InstMemory::InstMemory(const unsigned& initPc) {
+    pc = initPc;
+    memset(reg, 0, sizeof(unsigned) * 32);
+}
+
+LB::InstMemory::~InstMemory() {
+}
+
+void LB::InstMemory::init(const unsigned & initPc) {
+    pc = initPc;
+    memset(reg, 0, sizeof(unsigned) * 32);
+}
+
+bool LB::InstMemory::isValidAddress(const unsigned& opCode, const unsigned& c) {
+    if (opCode == 0x23U || opCode == 0x2B) {
+        return (c % 4) == 0;
+    }
+    else if (opCode == 0x21U || opCode == 0x25U || opCode == 0x29U) {
+        return (c & 1) == 0;
+    }
+    else {
+        return true;
+    }
+}
+
+bool LB::InstMemory::isValidAddress(const std::string& opCode, const unsigned& c) {
+    if (opCode == "lw" || opCode == "sw") {
+        return (c % 4) == 0;
+    }
+    else if (opCode == "lh" || opCode == "lhu" || opCode == "sh") {
+        return (c & 1) == 0;
+    }
+    else {
+        return true;
+    }
+}
+
+unsigned LB::InstMemory::getValueOfAddr(const unsigned& addr, const LB::InstMemLength& type) {
+    if (type == LB::InstMemLength::WORD) {
+        return reg[addr];
+    }
+    else if (type == LB::InstMemLength::HALFWORD) {
+        return reg[addr] & 0x0000FFFFU;
+    }
+    else {
+        return reg[addr] & 0x000000FFU;
+    }
+}
+
+void LB::InstMemory::setValueOfAddr(const unsigned& addr, const unsigned& val, const LB::InstMemLength& type) {
+    if (type == LB::InstMemLength::WORD) {
+        reg[addr] = val;
+    }
+    else if (type == LB::InstMemLength::HALFWORD) {
+        reg[addr] = val & 0x0000FFFFU;
+    }
+    else {
+        reg[addr] = val & 0x000000FFU;
+    }
+}
---
+++ InstMemory.h
@@ -0,0 +1,43 @@
+/*
+ * InstMemory.h
+ *
+ *  Created on: 2016/03/10
+ *      Author: LittleBird
+ */
+
+#ifndef INSTMEMORY_H_
+#define INSTMEMORY_H_
+
+#include <cstring>
+#include <string>
+
+namespace LB {
+
+enum class InstMemLength {
+    WORD, HALFWORD, BYTE
+};
+
+// 32 registers + pc
+class InstMemory {
+public:
+    InstMemory();
+    InstMemory(const unsigned& initPc);
+    virtual ~InstMemory();
+    // initialize with pc(default = 0)
+    void init(const unsigned& initPc = 0U);
+    //check whether the address is valid (or misaligned)
+    bool isValidAddress(const unsigned& opCode, const unsigned& c);
+    bool isValidAddress(const std::string& opCode, const unsigned& c);
+    // get reg value at addr
+    unsigned getValueOfAddr(const unsigned& addr, const InstMemLength& type);
+    // save reg value to addr
+    void setValueOfAddr(const unsigned& addr, const unsigned& val, const InstMemLength& type);
+
+private:
+    unsigned reg[32];
+    unsigned pc;
+};
+
+} /* namespace LB */
+
+#endif /* INSTMEMORY_H_ */
