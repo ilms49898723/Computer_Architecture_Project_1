39f703cf4efc190509ab3f115a035708c7fa5555
LittleBird <ilms49898723@gmail.com>
2016-03-11 01:55:12 +0900

    Add loadImageI, loadImageD

---
+++ InstSimulator.cpp
@@ -0,0 +1,33 @@
+/*
+ * InstSimulator.cpp
+ *
+ *  Created on: 2016/03/11
+ *      Author: LittleBird
+ */
+
+#include "InstSimulator.h"
+#include <cstdio>
+
+LB::InstSimulator::InstSimulator() {
+    instSet.clear();
+    mem.init();
+}
+
+LB::InstSimulator::~InstSimulator() {
+}
+
+void LB::InstSimulator::loadImageI(const unsigned* src, const unsigned& len, const unsigned& pc) {
+    mem.setPc(pc);
+    for (unsigned i = 0; i < len; ++i) {
+        instSet.push_back(LB::InstDecode::decodeInst(src[i]));
+        printf("load %s\n", LB::InstDecode::decodeInst(src[i]).toString().c_str());
+    }
+}
+
+// sp -> $29 == $(0x1D)
+void LB::InstSimulator::loadImageD(const unsigned* src, const unsigned& len, const unsigned& sp) {
+    mem.setValueOfAddr(0x1DU, sp, LB::InstMemLength::WORD);
+    for (unsigned i = 0; i < len; ++i) {
+        mem.setValueOfAddr(i, src[i], LB::InstMemLength::WORD);
+    }
+}
---
+++ InstSimulator.h
@@ -0,0 +1,33 @@
+/*
+ * InstSimulator.h
+ *
+ *  Created on: 2016/03/11
+ *      Author: LittleBird
+ */
+
+#ifndef INSTSIMULATOR_H_
+#define INSTSIMULATOR_H_
+
+#include "InstDecode.h"
+#include "InstMemory.h"
+#include "InstData.h"
+
+#include <vector>
+
+namespace LB {
+
+class InstSimulator {
+public:
+    InstSimulator();
+    virtual ~InstSimulator();
+    void loadImageI(const unsigned* src, const unsigned& len, const unsigned& pc);
+    void loadImageD(const unsigned* src, const unsigned& len, const unsigned& sp);
+
+private:
+    InstMemory mem;
+    std::vector<InstData> instSet;
+};
+
+} /* namespace LB */
+
+#endif /* INSTSIMULATOR_H_ */
