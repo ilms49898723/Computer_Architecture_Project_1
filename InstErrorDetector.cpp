ed5f57c74c17beeb52509bbcac4f82e3051e431a
LittleBird <ilms49898723@gmail.com>
2016-03-24 00:05:03 +0900

    add class InstErrorDetector

---
+++ InstErrorDetector.cpp
@@ -0,0 +1,56 @@
+/*
+ * InstErrorDetector.cpp
+ *
+ *  Created on: 2016/03/23
+ *      Author: LittleBird
+ */
+
+#include "InstErrorDetector.h"
+
+namespace LB {
+
+bool InstErrorDetector::isRegWritable(const unsigned& reg) {
+    return reg != 0U;
+}
+
+bool InstErrorDetector::isRegWritable(const InstDataBin& inst) {
+    return inst.getRt() != 0U;
+}
+
+bool InstErrorDetector::isRegWritable(const InstDataStr& inst) {
+    return inst.getRt() != "zero" && inst.getRt() != "0";
+}
+
+bool InstErrorDetector::isValidMemoryAddr(const unsigned& addr) {
+    return addr >= 0U && addr < 1024U;
+}
+
+bool InstErrorDetector::isOverflowed(const int& src0, const int& src1, const InstOpType& op) {
+    int a = src0;
+    int b = src1;
+    if (op == InstOpType::SUB) {
+        b *= (-1);
+    }
+    int result = a + b;
+    int signA = (a > 0) ? 1 : -1;
+    int signB = (b > 0) ? 1 : -1;
+    int signR = (result > 0) ? 1 : -1;
+    if (signA * signB < 0) {
+        return false;
+    }
+    return signA * signR < 0;
+}
+
+bool InstErrorDetector::isAlignedAddr(const unsigned& addr, const InstMemLen& type) {
+    if (type == InstMemLen::WORD) {
+        return (addr % 4) == 0;
+    }
+    else if (type == InstMemLen::HALFWORD) {
+        return (addr & 1) == 0;
+    }
+    else {
+        return true;
+    }
+}
+
+} /* namespace LB */
---
+++ InstErrorDetector.h
@@ -0,0 +1,44 @@
+/*
+ * InstErrorDetector.h
+ *
+ *  Created on: 2016/03/23
+ *      Author: LittleBird
+ */
+
+#ifndef INSTERRORDETECTOR_H_
+#define INSTERRORDETECTOR_H_
+
+#include "InstDataBin.h"
+#include "InstDataStr.h"
+#include "InstUtility.h"
+#include "InstMemory.h"
+
+namespace LB {
+
+// detect error
+// write to $0
+// overflow
+// memory addr overflow
+// data misaligned
+class InstErrorDetector {
+public:
+    // for argument with type InstDataBin, InstDataStr
+    // only check reg $rs
+    static bool isRegWritable(const unsigned& reg);
+    static bool isRegWritable(const InstDataBin& inst);
+    static bool isRegWritable(const InstDataStr& inst);
+
+    // check overflow
+    // check src0 "+" src1
+    static bool isOverflowed(const int &src0, const int &src1, const InstOpType& op);
+
+    // check memory address(should between 0~1023)
+    static bool isValidMemoryAddr(const unsigned& addr);
+
+    // check misaligned address
+    static bool isAlignedAddr(const unsigned& addr, const InstMemLen& type);
+};
+
+} /* namespace LB */
+
+#endif /* INSTERRORDETECTOR_H_ */
