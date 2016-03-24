15696461a96faabbfc454c1f78b3ef4cc2a6fd6d
LittleBird <ilms49898723@gmail.com>
2016-03-24 23:07:24 +0900

    delete unnecessary code

--- main.cpp
+++ main.cpp
@@ -26,25 +26,5 @@ int main(int argc, char **argv) {
     simulator.simulate(snapshot, error);
     fclose(snapshot);
     fclose(error);
-//    unsigned pc, len;
-//    pc = readWord();
-//    len = readWord();
-//    unsigned insts[11100];
-//    printf("len = %u\n", len);
-//    for (unsigned i = 0; i < len; ++i) {
-//        insts[i] = readWord();
-//    }
-//    unsigned dim[11100];
-//    unsigned dlen;
-//    unsigned sp;
-//    sp = readWord();
-//    dlen  = readWord();
-//    for (unsigned i = 0; i < dlen; ++i) {
-//        dim[i] = readWord();
-//    }
-//    LB::InstSimulator simulator;
-//    simulator.loadImageI(insts, len, pc);
-//    simulator.loadImageD(dim, dlen, sp);
-//    simulator.simulate(stdout, stderr);
     return 0;
 }
