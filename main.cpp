f0b06ac87879f4cb9ba8fd068595bb5fea83730f
LittleBird <ilms49898723@gmail.com>
2016-03-24 22:50:53 +0900

    use file io

--- main.cpp
+++ main.cpp
@@ -6,43 +6,45 @@
  */
 
 #include <cstdio>
+#include <cstdlib>
 #include "InstSimulator.h"
-
-inline unsigned readWord();
-inline unsigned readWordFromBin(FILE* fin);
+#include "InstImageReader.h"
 
 int main(int argc, char **argv) {
-    unsigned pc, len;
-    pc = readWord();
-    len = readWord();
-    unsigned insts[11100];
-    printf("len = %u\n", len);
-    for (unsigned i = 0; i < len; ++i) {
-        insts[i] = readWord();
-    }
-    unsigned dim[11100];
-    unsigned dlen;
-    unsigned sp;
-    sp = readWord();
-    dlen  = readWord();
-    for (unsigned i = 0; i < dlen; ++i) {
-        dim[i] = readWord();
-    }
+    unsigned ilen, dlen;
+    unsigned pc, sp;
+    unsigned inst[2048], memory[2048];
+    ilen = LB::InstImageReader::readImageI("iimage.bin", inst, &pc);
+    dlen = LB::InstImageReader::readImageD("dimage.bin", memory, &sp);
+
+    FILE *snapshot, *error;
+    snapshot = fopen("userSnapShot.rpt", "w");
+    error = fopen("userErrorDump.rpt", "w");
     LB::InstSimulator simulator;
-    simulator.loadImageI(insts, len, pc);
-    simulator.loadImageD(dim, dlen, sp);
-    simulator.simulate(stdout, stderr);
+    simulator.loadImageI(inst, ilen, pc);
+    simulator.loadImageD(memory, dlen, sp);
+    simulator.simulate(snapshot, error);
+    fclose(snapshot);
+    fclose(error);
+//    unsigned pc, len;
+//    pc = readWord();
+//    len = readWord();
+//    unsigned insts[11100];
+//    printf("len = %u\n", len);
+//    for (unsigned i = 0; i < len; ++i) {
+//        insts[i] = readWord();
+//    }
+//    unsigned dim[11100];
+//    unsigned dlen;
+//    unsigned sp;
+//    sp = readWord();
+//    dlen  = readWord();
+//    for (unsigned i = 0; i < dlen; ++i) {
+//        dim[i] = readWord();
+//    }
+//    LB::InstSimulator simulator;
+//    simulator.loadImageI(insts, len, pc);
+//    simulator.loadImageD(dim, dlen, sp);
+//    simulator.simulate(stdout, stderr);
     return 0;
-}
-
-inline unsigned readWord() {
-    unsigned a, b;
-    scanf("%x%x", &a, &b);
-    return (a << 16) | b;
-}
-
-inline unsigned readWordFromBin(FILE *fin) {
-    unsigned char input[4];
-    fread(input, sizeof(char), 4, fin);
-    return (input[0] << 24) | (input[1] << 16) | (input[2] << 8) | (input[3]);
 }
